bed3ef2783e72299bce9ebc09c7838ce312beecd
Nika Jorjoliani <nikajorjika1993@gmail.com>
2024-12-16 18:39:41 +0400

    Test: Add test for basket creation

--- tests/Pay/PayTest.php
+++ tests/Pay/PayTest.php
@@ -36,3 +36,37 @@ it('processes payment and returns response', function () {
         ->toHaveKey('redirect_url', 'https://example.com/redirect')
         ->toHaveKey('details_url', 'https://example.com/details');
 });
+
+it('can set custom basket array', function () {
+    $basket = [
+        ['name' => 'Product 1', 'quantity' => 1, 'unit_price' => 100.00],
+        ['name' => 'Product 2', 'quantity' => 2, 'unit_price' => 50.00],
+    ];
+
+    $this->pay->orderId('12345')->redirectUrl('https://example.com/status')->amount(200.00, 'GEL', $basket);
+
+    expect($this->pay->getPayload())->toMatchArray([
+        'callback_url' => config('bog-payment.callback_url'),
+        'external_order_id' => '12345',
+        'purchase_units' => [
+            'currency' => 'GEL',
+            'total_amount' => 200.00,
+            'basket' => [
+                [
+                    'name' => 'Product 1',
+                    'quantity' => 1,
+                    'unit_price' => 100.00,
+                ],
+                [
+                    'name' => 'Product 2',
+                    'quantity' => 2,
+                    'unit_price' => 50.00,
+                ],
+            ],
+        ],
+        'redirect_urls' => [
+            'fail' => 'https://example.com/status',
+            'success' => 'https://example.com/status',
+        ],
+    ]);
+});
